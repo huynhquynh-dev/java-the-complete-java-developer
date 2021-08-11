918de851eabe9b0681fcd3827e71e024d0cdcf98
huynhquynh-dev <huynhquynh-dev>
2021-08-11 15:47:24 +0700

    indexOf(), lastIndexOf()

---
+++ assets/scripts/191.js
@@ -0,0 +1,9 @@
+const hobbies = ["Sports", "Cooking", "Cooking"];
+
+// Tìm theo value và trả về vịt trí index đầu tiên đc tìm thấy
+const option = hobbies.indexOf("Cooking");
+console.log(option);
+
+// Tìm index cuối cùng
+const result = hobbies.lastIndexOf("Cooking");
+console.log(result);
