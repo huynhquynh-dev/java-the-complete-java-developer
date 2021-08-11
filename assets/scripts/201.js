8952fc6ad301504573167409a240ffb7a603468f
huynhquynh-dev <huynhquynh-dev>
2021-08-11 16:22:50 +0700

    split(), join()

---
+++ assets/scripts/201.js
@@ -0,0 +1,7 @@
+const strings = "abc;20,99;40000";
+const resultArray = strings.split(";");
+console.log(resultArray);
+
+const array = ["Spring", "Boot"]
+const resultJoin = array.join(" ")
+console.log(resultJoin)
