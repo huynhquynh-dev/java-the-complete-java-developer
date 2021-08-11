cca54f8a2d69856144d58f19507c2abb55a8283b
huynhquynh-dev <huynhquynh-dev>
2021-08-11 16:05:03 +0700

    sort()

---
+++ assets/scripts/196.js
@@ -0,0 +1,9 @@
+const prices = [1, 2, 3, 4, 5];
+
+const sortedPrices = prices.sort((a, b) => {
+  if (a < b) return 1;
+  else if (a === b) return 0;
+  else return -1;
+});
+
+console.log(object);
