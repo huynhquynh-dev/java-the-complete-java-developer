beb8e00ddea6838af4b09c8360af7a45607a2b27
huynhquynh-dev <huynhquynh-dev>
2021-08-11 15:29:23 +0700

    splice()

---
+++ assets/scripts/188.js
@@ -0,0 +1,7 @@
+const hobbies = ["Sports", "Cooking"];
+
+// Thêm phần tử vào mảng theo chỉ số index
+hobbies.splice(1, 0, "Good Food")
+
+// Xóa 2 phần tử trong mảng bắt đầu ở index = 1
+hobbies.splice(1, 2)
